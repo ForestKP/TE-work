013ba3b3d8daec9690d43a64f4d7f57226f40802
Forest Perkins <forestkane@gmail.com>
2022-06-20 07:37:37 -0500

    mod2 day4 exercise

--- module-2/04_INSERT_UPDATE_and_DELETE/exercise/Exercises/12_add_collection.sql
+++ module-2/04_INSERT_UPDATE_and_DELETE/exercise/Exercises/12_add_collection.sql
@@ -1,2 +1,14 @@
 -- 12. Create a "Bill Murray Collection" in the collection table. For the movies that have Bill Murray in them, set their collection ID to the "Bill Murray Collection". (1 row, 6 rows)
 
+INSERT INTO collection (collection_name)
+VALUES ('Bill Murray collection');
+
+UPDATE movie
+SET collection_id = (SELECT collection_id FROM collection WHERE collection_name = 'Bill Murray Collection')
+WHERE movie_id IN (
+Select ma.movie_id
+FROM person p 
+JOIN movie_actor ma 
+ON p.person_id = ma.actor_id
+WHERE person_name = 'Bill Murray'
+)
