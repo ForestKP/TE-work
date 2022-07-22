264897d012796a9ecd9d11df8329321036384d29
Forest Perkins <forestkane@gmail.com>
2022-07-22 07:50:58 -0500

    mod3domexercise

--- module-3/08_DOM/exercise/shopping-list/js/shopping-list.js
+++ module-3/08_DOM/exercise/shopping-list/js/shopping-list.js
@@ -1,24 +1,54 @@
 // add pageTitle
-
+let pageTitle = 'My Shopping List';
 // add groceries
-
+let groceries = [
+  { id: 1, item: 'Bota Box - Cab', completed: false },
+  { id: 2, item: 'Bota Box - Pinot Grigio', completed: false },
+  { id: 3, item: 'Taco Shells', completed: false },
+  { id: 4, item: 'Ground Beef', completed: false },
+  { id: 5, item: 'Ground Turkey', completed: false },
+  { id: 6, item: 'Maters', completed: false },
+  { id: 7, item: 'Alfredo Sauce', completed: false },
+  { id: 8, item: 'Bread', completed: false },
+  { id: 9, item: 'Chicken Thighs', completed: false },
+  { id: 10, item: 'Corn', completed: false }
+];
 /**
  * This function will get a reference to the title and set its text to the value
  * of the pageTitle variable that was set above.
  */
 function setPageTitle() {}
-
+{
+  const heading = document.getElementById('title')
+  heading.innerText = pageTitle;
+}
 /**
  * This function will loop over the array of groceries that was set above and add them to the DOM.
  */
 function displayGroceries() {}
+{
+  const ul = document.getElementById('groceries');
 
+  groceries.forEach(items => 
+    {
+      const li = document.createElement('li');
+      li.innerText = items.item;
+      ul.appendChild(li);
+    })
+}
 /**
  * This function will be called when the button is clicked. You will need to get a reference
  * to every list item and add the class completed to each one
  */
 function markCompleted() {}
+{
+  const groceryItems = document.querySelectorAll('li');
 
+  groceryItems.forEach((item) => 
+    {
+      item.setAttribute('class', 'completed');
+    })
+}
 setPageTitle();
 
 displayGroceries();
