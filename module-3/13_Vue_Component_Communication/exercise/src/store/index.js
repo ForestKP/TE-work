d0b23a06eea994daa0b24b932ad8f7ffcbbfc0a5
Forest Perkins <forestkane@gmail.com>
2022-08-02 07:54:26 -0500

    homework and sht

--- module-3/13_Vue_Component_Communication/exercise/src/store/index.js
+++ module-3/13_Vue_Component_Communication/exercise/src/store/index.js
@@ -2,38 +2,44 @@ import Vue from "vue";
 import Vuex from "vuex";
 
 Vue.use(Vuex);
-//add code here
-``` JSON
-books: [
-  {
-    title: "Kafka on the Shore",
-    author: "Haruki Murakami",
-    read: false,
-    isbn: "9781400079278"
-  },
-  {
-    title: "The Girl With All the Gifts",
-    author: "M.R. Carey",
-    read: true,
-    isbn: "9780356500157"
-  },
-  {
-    title: "The Old Man and the Sea",
-    author: "Ernest Hemingway",
-    read: true,
-    isbn: "9780684830490"
-  },
-  {
-    title: "Le Petit Prince",
-    author: "Antoine de Saint-Exupéry",
-    read: false,
-    isbn: "9783125971400"
-  }
-]
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
+  state: {
+    books: [
+      {
+        title: "Kafka on the Shore",
+        author: "Haruki Murakami",
+        read: false,
+        isbn: "9781400079278"
+      },
+      {
+        title: "The Girl With All the Gifts",
+        author: "M.R. Carey",
+        read: true,
+        isbn: "9780356500157"
+      },
+      {
+        title: "The Old Man and the Sea",
+        author: "Ernest Hemingway",
+        read: true,
+        isbn: "9780684830490"
+      },
+      {
+        title: "Le Petit Prince",
+        author: "Antoine de Saint-Exupéry",
+        read: false,
+        isbn: "9783125971400"
+      }
+    ]   
+  },
+  mutations: {
+    SET_READ_STATUS(state, payload) {
+      payload.book.read = payload.value;
+    },
+    SAVE_BOOK(state, book) {
+      state.books.push(book);
+    }
+  },
   actions: {},
   modules: {},
   strict: true
