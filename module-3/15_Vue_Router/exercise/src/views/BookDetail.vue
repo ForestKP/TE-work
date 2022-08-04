26441ceeb2988c84533d8f22a7e27b920d3efff0
Forest Perkins <forestkane@gmail.com>
2022-08-04 16:47:58 -0500

    try3mod

--- module-3/15_Vue_Router/exercise/src/views/BookDetail.vue
+++ module-3/15_Vue_Router/exercise/src/views/BookDetail.vue
@@ -3,7 +3,7 @@
 </template>
 
 <script> // JavaScript
-import BookDetails from '@/components/BookDetails';
+import BookDetails from '../components/BookDetails';
   export default { // Expose data to external processes
     components: { // list of vue components used in this one
         BookDetails
